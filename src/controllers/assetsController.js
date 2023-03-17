e28b8a87b94fc929bd2977b42fb392c028aae8f1
Santiago Rodriguez <83855468+Ariodriguez@users.noreply.github.com>
2023-03-17 03:34:20 -0300

    todos los endpoints terminados

--- src/controllers/assetsController.js
+++ src/controllers/assetsController.js
@@ -18,9 +18,24 @@ const getAssets = async (req, res) => {
 }
 
 //Get Assets by Employee Id
+const getAssetsByEmployeeId = async (req,res) => {
+    try {
+        const { employee_id } = req.params
+        const connection = await getConnection()
+        await connection.query('SELECT * FROM assets WHERE employee_id = ?',
+        employee_id,
+        function (err, rows) 
+            { res.send(rows); 
+            console.log("result: ", rows); 
+        })
+        
+    } catch (error) {
+        res.status(500)
+        console.log("error: ", error );
+    }
+}
 
-
-//Get Assets by Id
+//Get Assets By Id
 const getAssetsById = async (req, res) => {
     try {
         const { assets_id } = req.params
@@ -38,7 +53,7 @@ const getAssetsById = async (req, res) => {
     }
 }
 
-//Get Assets Paginated
+//Get Paginated Assets
 const getPaginatedAssets = async (req, res) => {
     const page = req.query.page || 1;
     const limit = req.query.limit || 10;
@@ -59,15 +74,15 @@ const getPaginatedAssets = async (req, res) => {
 }
 
 
-//Post => Add Assets
+//Post => Create Asset
 const addAsset = async (req, res) =>{
-    const{ name, type, code, marca, description, purchase_date, assets_id } = req.body
+    const{ name, type, code, marca, description, purchase_date, assets_id, employee_id } = req.body
     if (!!name && !!type && !!marca) {
         try {
             const purchase_date = moment().format(purchase_date)
             const connection = await getConnection()
             const assets = {
-                name, type, code, marca, description, purchase_date, assets_id 
+                name, type, code, marca, description, purchase_date, assets_id, employee_id 
             }
             await connection.query('INSERT INTO assets SET ?', assets,
             function (err, result) 
@@ -84,7 +99,7 @@ const addAsset = async (req, res) =>{
     }
 }
 
-//Update Assets
+//Update Asset
 const updateAsset = async (req, res) => {
     const { assets_id } = req.params
     const { name, type, code, marca, description, purchase_date } = req.body
@@ -126,5 +141,6 @@ export const methods = {
     getPaginatedAssets,
     addAsset,
     updateAsset,
-    deleteAsset
+    deleteAsset,
+    getAssetsByEmployeeId
 }
--- src/routes/assetsRoutes.js
+++ src/routes/assetsRoutes.js
@@ -6,8 +6,10 @@ const router = Router();
 router.get('/get', assetsController.getAssets);
 router.get('/get/:assets_id', assetsController.getAssetsById);
 router.get('/pag', assetsController.getPaginatedAssets);
+router.get('/employees/:employee_id/assets', assetsController.getAssetsByEmployeeId)
 router.post('/add', assetsController.addAsset);
 router.put('/update/:assets_id', assetsController.updateAsset)
 router.delete('/delete/:assets_id', assetsController.deleteAsset)
+
 
 export default router
